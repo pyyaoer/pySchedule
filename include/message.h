b4ca258b83b0f51ea009e4ce14e8f7657473b428
pyyaoer <pyyaoer@gmail.com>
2018-11-03 22:56:11 +0800

    Add create time for message class

--- include/message.h
+++ include/message.h
@@ -5,28 +5,32 @@
   TypeName(const TypeName&); \
   void operator=(const TypeName&)
 
-enum eMessageType{ BASIC = 0 };
+enum eMessageType{ BASIC = 0, REQUEST };
 
 class Message {
  public:
   explicit Message(const char* data) {
-    sscanf(data, "%d %d %d", &src_port_, &dst_port_, &type_);
+    sscanf(data, "%d %d %d %lld", &src_port_, &dst_port_, &type_, &create_time_);
   };
   explicit Message(short src_port, short dst_port, eMessageType type)
-    : src_port_(src_port), dst_port_(dst_port), type_(type) {}
+    : src_port_(src_port), dst_port_(dst_port), type_(type) {
+      create_time_ = time(0);
+    }
   ~Message() {};
 
   short GetSrcPort() { return src_port_; }
   short GetDstPort() { return dst_port_; }
   std::string ToString() { 
     std::stringstream ret;
-    ret << src_port_ << " " << dst_port_ << " " << type_ << "\0";
+    ret << src_port_ << " " << dst_port_ << " " << type_ 
+        << " " << create_time_ << "\0";
     return ret.str();
   }
 
  private:
   short src_port_;
   short dst_port_;
+  long long create_time_;
   eMessageType type_;
 
   DISALLOW_COPY_AND_ASSIGN(Message);
